e299f9f2cdfd06cff1645aedf7da863ea20c73f5
Hannah <di.hannah99@gmail.com>
2023-02-23 17:22:54 +0000

    included traktapi

--- config.js
+++ config.js
@@ -1,5 +1,14 @@
-var apikeys = {
-    MY_KEY : "123456",
-    SECRET_KEY : "56789",
-    KEY_2 : "101010"
+// stores the API keys received from trakt.tv, will not be pushed to github
+var traktapi = {
+    username: "movieLover123",
+    clientId: "10eb076c49dd7eccf220c8e3c46bcf8ea3453a5a37cbde13af78e641740adc99",
+    clientSecret : "2f8e5d36561b76db67d86e06a0fe69975375da9fcabf9e2ed14ea79c109aa4d2",
+    token : "",
+    token2: "98357eccc439b85b67671100fb00b894a4e8626c0b28da2c0bcddf32289c307c"
    }
+
+var imdbapi = {
+    key1: "k_6wmi1gqg", // Hannah
+    key2: "", // Julia
+    key3: "k_s6o9v1tp" // Celina
+}
---
+++ login.js
@@ -0,0 +1,72 @@
+// variables to store the api-keys received trakt.tv, stored in config.js
+var clientId = "";
+var clientSecret = "";
+var token = "";
+var username = traktapi.username;
+var token2 = traktapi.token2
+var redirectUri = "http://127.0.0.1:5500/movie-webapp/";
+// var code = "16b467e1d2594add5aa309f72e88a47a031675b340e2f677c3d14352be6b070e";
+
+/** access apikeys stored in config2.js and assign to variables */
+function getCredentials() {
+  clientId = traktapi.clientId
+  clientSecret = traktapi.clientSecret
+}
+
+/** force login */
+function login() {
+  console.log(clientId)
+  url = `https://api.trakt.tv/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=http%3A%2F%2F127.0.0.1%3A5500%2Fmovie-webapp%2F`
+  window.location.href = url
+}
+
+/** get the code that is in the url */
+function getCode() {
+  const code = new URLSearchParams(window.location.search).get("code")
+
+  if (code === null) {
+    console.log("There is no code yet");
+  } else {
+    console.log("Received a code");
+    
+    // call function that replaces the code received now with a token valid for 3 months
+    getToken(code);
+  }
+}
+
+window.addEventListener('load', getCredentials)
+window.addEventListener('load', getCode)
+
+/** replace code with token */
+function getToken(code) {
+  var request = new XMLHttpRequest();
+
+  request.open('POST', 'https://api.trakt.tv/oauth/token');
+
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+    var response = JSON.parse(this.responseText);
+    token = response["access_token"];
+
+    traktapi.token = token;
+    console.log(traktapi.token)
+  }
+};
+
+    var body = {
+    'code': code,
+    'client_id': clientId,
+    'client_secret': clientSecret,
+    'redirect_uri': redirectUri,
+    'grant_type': 'authorization_code'
+};
+
+request.send(JSON.stringify(body));
+}
+
+
