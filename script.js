27277787d534c165108df79611bbffe7b4959b28
Hannah <di.hannah99@gmail.com>
2023-02-23 17:22:54 +0000

    included traktapi

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var imdbApiKey = imdbapi.key3
+var watchedArr = []
+
 // colors with transparency 30-50
 // https://www.w3schools.com/colors/colors_names.asp
 const genreColors = {
@@ -27,21 +30,11 @@ const genreColors = {
     // Western: "#DEB88750" // semi-transparent burly wood
 };
 
-// index.html, top250.json
-// later implement API calls, get the API keys from config file (not uploaded, configured in .gitignore)
-var myKey = " ";
-var url = "https://www.whatever.com/?query&sig=" + myKey;
 
-
-function getAPIkey() {
-    fetch('config.js').then(function (config) {
-        console.log('API key:', apikeys.MY_KEY);
-    });
-
-}
-
+// ********** index.html, top250.json ********** 
+// Gets the top250 movies from the json file and assigns the response data to movieData
 async function getMoviesFromJsonFile() {
-    // other way to write promise (needs async keyword). Gets the top250 movies from the json file and assigns the response data to movieData
+    // other way to write promise (needs async keyword). 
     const response = await fetch("json_files/top250.json");
     const movieData = await response.json();
     
@@ -49,11 +42,15 @@ async function getMoviesFromJsonFile() {
     populateMovies(movieData);
 }
 
+/**
+ * populateMovies takes argument data.
+ * it populates index.html top250 id with movie data
+ * @param {json} data - JSON file
+ */
 function populateMovies(data) {
     console.log(data);
     const section = document.querySelector('#top250'); // select the sections
     const movies = data.items; 
-    console.log(movies);
 
     // loop through the elements of the movies array, get title and image from JSON to display on webapp
     for (const movie of movies) {
@@ -77,22 +74,25 @@ function populateMovies(data) {
     }
 }
 
-// in_theaters.html, in_theaters.json
+// ********** in_theaters.html, in_theaters.json **********
+// Gets the movies in theaters from the json file and assigns the response data to movieData
 async function getInTheatersFromJsonFile() {
-    // other way to write promise (needs async keyword). Gets the movies in theaters from the json file and assigns the response data to movieData
+    // other way to write promise (needs async keyword). 
     const response = await fetch("json_files/in_theaters.json");
     const theaterData = await response.json();
-    console.log("called Theater func");
+
     // call the function populateMovies 
     populateTheaterGallery(theaterData);
-    console.log(mykey)
 }
 
+/**
+ * populateTheaterGallery takes argument data.
+ * it populates in_theaters.html gallery class with data about movies currently in theaters
+ * @param {json} data - JSON file
+ */
 function populateTheaterGallery(data) {
-    console.log(data);
     const section = document.querySelector('.gallery'); // select the sections
     const movies = data.items; 
-    console.log(movies);
 
     // loop through the elements of the movies array, get title and image from JSON to display on webapp
     for (const movie of movies) {
@@ -143,22 +143,117 @@ function populateTheaterGallery(data) {
             movGenre.style.display = "inline-block";
             movGenre.style.margin = "2px";
             
-
+            // append the genre li elements to the genreList ul element
             genreList.appendChild(movGenre);            
-
-            // console.log(genreColors.genreName)
-            
-            // const movGenre = document.createElement('p');
-            // movGenre.textContent = genre.key;
         }
 
-        
-        // movieCard.appendChild(movGenre);
-        movieCard.appendChild(genreList);
+        // append the element to the section
+        movieCard.appendChild(genreList); // append the genreList ul element to movieCard article element
+        section.appendChild(movieCard); // append the movieCard article to the section selector (selected .gallery)
+    }
+}
 
-        section.appendChild(movieCard);
+
+/** testing getWatched function of trakt api */
+function getWatched() {
+    var request = new XMLHttpRequest();
+    request.open('GET', `https://api.trakt.tv/users/${username}/watched/movies`);
+
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('trakt-api-version', '2');
+    request.setRequestHeader('Authorization', `Bearer ${token2}`);
+    request.setRequestHeader('trakt-api-version', '2');
+    request.setRequestHeader('trakt-api-key', clientId);
+
+  
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.responseText);
+            var watched = JSON.parse(this.responseText)
+
+        // for every movie in watched list, get more data from IMDB
+        for (const movie of watched) {
+            imdbId = movie.movie.ids.imdb
+            getMovData(imdbId)
+        }
+    }
+  };
+  request.send();
+        
+  populateWatchedGallery();
+}
+
+/** get movie or series data from imdb api*/
+function getMovData(imdbId) {
+    url = `https://imdb-api.com/en/API/Title/${imdbApiKey}/${imdbId}/FullActor,FullCast,Posters,Images,Ratings,`
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {watchedArr.push(data)}); // save the response in watchedArr
+}
+
+function populateWatchedGallery() {
+    console.log("in this function finally")
+    console.log(watchedArr)
+    const section = document.querySelector('.watched-gallery'); // select the sections
+    const movies = watchedArr; 
+    
+    for (const movie of movies) {
+        const movieCard = document.createElement('article'); // create element article that all new elements will be appended to
+        const genreList = document.createElement('ul'); // create element list that will contain the genre tags
+        // later move to CSS
+        genreList.style.padding = "0px";
+        genreList.style.width = "130px";
+        genreList.style.margin = "0px";
+
+        // cover poster of the movie
+        const movImg = document.createElement('img');
+        movImg.src = movie.image;
+        // later move to CSS
+        movImg.style.width = "130px";
+        movImg.style.height = "auto";
+        movImg.style["margin-top"] = "0px";
+        movieCard.appendChild(movImg);
+
+        // movie title
+        const movTitle = document.createElement('p'); // creates new element
+        movTitle.textContent = `${movie.title}`; // fill the p element with the title
+        // later move to CSS
+        movTitle.style.width = "130px";
+        movTitle.style.height = "50px";
+        movTitle.style["text-align"] = "center";
+        movTitle.style["font-size"] = "12px";
+        movTitle.style["margin-top"] = "2px";
+        movTitle.style["margin-bottom"] = "0px";
+        movieCard.appendChild(movTitle);
+
+        // movie genre in little tags that change color according to genre
+        for (const genre of movie.genreList) {
+            const movGenre = document.createElement('li'); // creates new element
+            genreName = genre.key;
+            movGenre.textContent = genreName;
+
+            // get color mapping from genreColors class
+            const genreColor = genreColors[genreName];
+            // later move to CSS
+            movGenre.style["background-color"] = genreColor;
+            movGenre.style["border-radius"] = "10px";
+            movGenre.style["font-size"] = "8px";
+            // movGenre.style.width = "60px";
+            movGenre.style["text-align"] = "center";
+            movGenre.style.padding = "2px 5px";
+            movGenre.style["list-style"] = "none";
+            movGenre.style.display = "inline-block";
+            movGenre.style.margin = "2px";
+            
+            // append the genre li elements to the genreList ul element
+            genreList.appendChild(movGenre);            
+        }
 
         // append the element to the section
-
+        movieCard.appendChild(genreList); // append the genreList ul element to movieCard article element
+        section.appendChild(movieCard); // append the movieCard article to the section selector (selected .gallery)
     }
 }
